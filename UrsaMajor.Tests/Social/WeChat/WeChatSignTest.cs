7a6c075eb79941652d433a07d6ea8fc6882f5d04
AceBearXP <acebearxp@msn.com>
2019-10-16 22:43:05 +0800

    init

---
+++ UrsaMajor.Tests/Social/WeChat/WeChatSignTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrsaMajor.Social.WeChat;
+
+namespace UrsaMajor.Tests.Social.WeChat
+{
+    [TestClass]
+    public class WeChatSignTest
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            const string key = "0123456789ABCDEF";
+            var objSign = new WeChatSign(key);
+            objSign.add("mchid", "012345679");
+            objSign.add("total_fee", "100");
+            objSign.add("out_trade_no", "M2401C1234560258");
+            objSign.add("type", null);
+            objSign.add("body", "优惠码");
+
+            string signed = objSign.calc();
+            Assert.IsNotNull(signed);
+        }
+
+        [TestMethod]
+        public async Task RemoteTest()
+        {
+            var dictArgs = new Dictionary<string, string>();
+            dictArgs.Add("mchid", "1534793921");
+            dictArgs.Add("total_fee", "1");
+            dictArgs.Add("out_trade_no", "M1234C123456790");
+            dictArgs.Add("type", String.Empty);
+            dictArgs.Add("body", "优惠码");
+            dictArgs.Add("attach", "MARK001");
+
+            const string key = "8JGf15JuTWEyUa57";
+            var objSign = new WeChatSign(key);
+            foreach (var x in dictArgs) {
+                objSign.add(x.Key, x.Value);
+            }
+            string sign = objSign.calc();
+            dictArgs.Add("sign", sign);
+            FormUrlEncodedContent body = new FormUrlEncodedContent(dictArgs);
+            
+            HttpClient httpClient = new HttpClient();
+            var post = await httpClient.PostAsync("https://payjs.cn/api/native", body);
+            var data = await post.Content.ReadAsStringAsync();
+
+            Assert.IsNotNull(data);
+
+        }
+    }
+}
---
+++ UrsaMajor/Social/WeChat/WeChatSign.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace UrsaMajor.Social.WeChat
+{
+    /// <summary>
+    /// 微信参数签名算法
+    /// </summary>
+    public class WeChatSign
+    {
+        /// <summary>
+        /// 密钥
+        /// </summary>
+        private string _key;
+
+        /// <summary>
+        /// 待参加签名的参数
+        /// </summary>
+        private SortedDictionary<string, string> _dictArgs = new SortedDictionary<string, string>();
+
+        /// <summary>
+        /// 构造微信签名算法对象
+        /// </summary>
+        /// <param name="key">密钥</param>
+        public WeChatSign(string key) {
+            _key = key;
+        }
+
+        public void clear()
+        {
+            _dictArgs.Clear();
+        }
+
+        /// <summary>
+        /// 追加参数
+        /// </summary>
+        /// <param name="key">参数键名</param>
+        /// <param name="value">参数键值</param>
+        public void add(string key, string value)
+        {
+            if(!String.IsNullOrWhiteSpace(value))
+                _dictArgs.Add(key, value);
+        }
+
+        /// <summary>
+        /// 计算签名
+        /// </summary>
+        /// <returns></returns>
+        public string calc()
+        {
+            // 对参数按照key=value的格式，并按照参数名ASCII字典序排序
+            StringBuilder sbBuf = new StringBuilder();
+            foreach (var k in _dictArgs.Keys) {
+                string v = _dictArgs[k];
+                sbBuf.Append($"{k}={v}&");
+            }
+
+            // 拼接密钥
+            sbBuf.Append($"key={_key}");
+
+            // md5
+            var buf = Encoding.UTF8.GetBytes(sbBuf.ToString());
+            var md5 = MD5.Create().ComputeHash(buf);
+            sbBuf.Clear();
+            foreach (var b in md5) {
+                sbBuf.AppendFormat("{0:X2}", b);
+            }
+
+            return sbBuf.ToString();
+        }
+    }
+}
